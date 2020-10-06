ca167fd73958ef286b1dc7b185a99335fc3d2bd4
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-06 20:37:10 +0200

    Corrections

--- src/Pagination.js
+++ src/Pagination.js
@@ -4,7 +4,7 @@ export default function Pagination({ gotoNextPage, gotoPrevPage }) {
   return (
     <div>
       {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
-      {gotoPrevPage && <button onClick={gotoPrevPage}>Next</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
     </div>
   );
 }
